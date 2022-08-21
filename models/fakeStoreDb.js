8ac0c41754b4ee038c521966f1cda53c023585d6
Artemio <artemiolucero@gmail.com>
2022-08-20 22:36:23 -0300

    Minor changes on Model

--- models/fakeStoreDb.js
+++ models/fakeStoreDb.js
@@ -1,5 +1,5 @@
 const fetch = (...args) =>
-import ('node-fetch').then(({ default: fetch }) => fetch(...args))
+    import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
 const getProducts = () => {
     return fetch('https://fakestoreapi.com/products')
@@ -8,13 +8,14 @@ const getProducts = () => {
 
 const getCarts = () => {
     return fetch('https://fakestoreapi.com/carts')
-     .then(res => res.json());
+        .then(res => res.json());
 }
 
 const getCategory = () => {
     return fetch('https://fakestoreapi.com/products/categories')
         .then(res => res.json());
 }
+
 const getCategoriesId = (category) => {
     return fetch(`https://fakestoreapi.com/products/category/${category}`)
         .then(res => res.json());
@@ -25,30 +26,35 @@ const getUsers = () => {
         .then(res => res.json());
 }
 
-const getPropCategories = async() =>{
+const getUserId = (id) => {
+    return fetch(`https://fakestoreapi.com/users/${id}`)
+        .then(res => res.json());
+}
+
+const getPropCategories = async () => {
     let categories = await getCategory();
 
     const products = await Promise.all(
-    categories.map(async (category) => {
-        return {
-        category,
-        products: await getCategoriesId(category),
-        };
-    })) 
+        categories.map(async (category) => {
+            return {
+                category,
+                products: await getCategoriesId(category),
+            };
+        }))
     return products
 };
 
-const getPropCart = async() => {
+const getPropCart = async () => {
     let carts = await getCarts();
     return carts
 };
 
-const getMappedProducts = async() => {
+const getMappedProducts = async () => {
     let products = await getProducts();
     products = products.map((product) => ({
-    id: product.id,
-    title: product.title,
-    price: product.price,
+        id: product.id,
+        title: product.title,
+        price: product.price,
     }));
     return products;
 };
@@ -57,11 +63,12 @@ const storeDb = {
     getProducts,
     getCarts,
     getUsers,
+    getUserId,
     getCategory,
     getCategoriesId,
     getPropCategories,
     getPropCart,
     getMappedProducts,
-  };
-  
+};
+
 module.exports = storeDb;
